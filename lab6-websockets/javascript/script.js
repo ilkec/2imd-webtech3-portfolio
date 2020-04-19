fe3f3f4214096789b80e9142f878d1f212c5a858
Ilke Cauwenbergh <r0449955@student.thomasmore.be>
2020-04-19 20:39:40 +0200

    lab 6 - added javascript file

---
+++ lab6-websockets/javascript/script.js
@@ -0,0 +1,12 @@
+primus = Primus.connect("http://localhost:3000", {
+    reconnect: {
+        max: Infinity // Number: The max delay before we try to reconnect.
+      , 
+      min: 500 // Number: The minimum delay before we try reconnect.
+      , 
+      retries: 10 // Number: How many times we should try to reconnect.
+    }
+  });
+
+
+
